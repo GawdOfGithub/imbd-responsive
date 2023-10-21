02712e34e765591c0e871f93e90790a8d54b40e2
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-21 06:23:55 +0530

    Saving progress on this branch: applying_cdn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
-
+import { cn } from "@/lib/utils"
+import { buttonVariants } from /components/ui/button"
+import { Icons } from "@/components/icons"
+import { UserAuthForm } from "@/components/user-auth-form"
+import { Link } from 'react-router-dom'
 export default function SignIn() {
   return (
-    <div className='bg-blue text-5xl  flex flex-col justify-center items-center text-black z-50 '> SignIn</div>
+    // <div className='bg-blue text-5xl  flex flex-col justify-center items-center text-black z-50 '> SignIn</div>
+    <div className="container flex h-screen w-screen flex-col items-center justify-center">
+      <Link to="/"
+      
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <>
+          <Icons.chevronLeft className="mr-2 h-4 w-4" />
+          Back
+        </>
+      </Link>
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <Icons.logo className="mx-auto h-6 w-6" />
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Welcome back
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email to sign in to your account
+          </p>
+        </div>
+        <UserAuthForm />
+        <p className="px-8 text-center text-sm text-muted-foreground">
+          <Link
+            href="/register"
+            className="hover:text-brand underline underline-offset-4"
+          >
+            Don&apos;t have an account? Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
