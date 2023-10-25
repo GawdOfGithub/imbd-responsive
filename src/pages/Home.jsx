8d998c63e418297a040612dbd0aef2ea6da8cf58
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-25 15:09:22 +0530

    Progress saved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
-
-import { MovieList } from '../Components/MovieList'
 import React from 'react'
+import Theme2 from '../Components/Theme2'
 
-export default function Home () {
- return(
-  <>
-<div className='h-content min-h-screen w-screen bg-gradient-to-r from-cyan-800 '>
-  <MovieList/>
-</div> 
-  </>
- )
+export default function Home() {
+  return (
+    <div><Theme2/></div>
+  )
 }
