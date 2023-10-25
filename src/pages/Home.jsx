acf1b03ad34ff3bf0e37d57fd6555ff6bb502640
Anurag Bhandari <anuragbhandari77736@gmail.com>
2023-10-25 15:18:32 +0530

    Saving progress on this branch: master

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+
+import { MovieList } from '../Components/MovieList'
+
+
+export default function Home () {
+   //using the context
+  
+ 
+ return(
+  <>
+
+<div className='h-content min-h-screen w-screen bg-gradient-to-r from-cyan-800 '>
+  
+ 
+  <MovieList/>
+  
+</div> 
+  </>
+ )
+}
